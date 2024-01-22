3fd16e84262ac0d982fd9d20395392a10ca24d49
qkrwhdah03 <qkrwhdah03@naver.com>
2024-01-22 19:33:03 +0900

    fix phaser dependency

---
+++ public/map_tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="map_tile" tilewidth="32" tileheight="32" tilecount="324" columns="18">
+ <image source="map_tile.png" width="576" height="576"/>
+</tileset>
